850a3a23262ec84b320e4c9a8f14a7c0b688d86e
Minecatl1 <140106910+Minecatl1@users.noreply.github.com>
2025-07-05 17:36:04 -0500

    Delete start.sh

--- start.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Start message
-echo "Starting NeoForge server with ${MIN_RAM}/${MAX_RAM} RAM..."
-
-# Launch server
-exec java -Xms${MIN_RAM} -Xmx${MAX_RAM} -jar "${SERVER_JAR}" nogui
